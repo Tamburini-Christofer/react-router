d1fec45ef06d3cd77c212914b40646deb5b0206b
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-06 14:26:17 +0200

    Creazione pagina Chi Siamo

--- src/pages/ChiSiamo.jsx
+++ src/pages/ChiSiamo.jsx
@@ -1,9 +1,34 @@
 const ChiSiamo = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  return (
+    <>
+      <div className="containerWho">
+        <div className="presentation who">
+          <h2>Chi siamo</h2>
+          <p>
+            Nel cuore pulsante della rete, tra luci al neon e ombre digitali,
+            nasce NeonBazaar: un mercato online dove tecnologia, estetica
+            futuristica e curiosità si fondono. <br />
+            <br />
+            Non vendiamo solo prodotti: offriamo esperienze. <br />
+            <br />
+            Ogni oggetto che trovi qui è scelto per chi vive sul confine tra
+            realtà e immaginazione, chi ama l’arte del cyberpunk e il fascino
+            del possibile. <br />
+            <br />
+            Siamo un team di sognatori digitali, hacker di stile e collezionisti
+            di meraviglie, pronti a trasformare il tuo shopping in un viaggio
+            attraverso mondi alternativi. <br />
+            <br />
+            Dalla gadgetistica high-tech agli accessori più stravaganti, da noi
+            ogni acquisto accende una scintilla di futuro. <br />
+            <br />
+            Benvenuto in NeonBazaar – dove il futuro non è un sogno, ma
+            un’esperienza che puoi toccare.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ChiSiamo;
