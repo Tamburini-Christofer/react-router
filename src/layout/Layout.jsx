1268734f3cbeb79debf1251621e22d151cf0220d
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-06 13:06:44 +0200

    Creazione layout pagina

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,15 @@
+import NavBar  from "../components/NavBar";
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
     return (
         <>
-            
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <Outlet />
+            </main>
         </>
     )
 }
