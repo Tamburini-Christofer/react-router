c9f48eadd2ca8f21c07733086dd2d0032bc97cd4
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-06 13:04:05 +0200

    Creazione NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+const linkPage = [
+    { route: '/', label: 'HomePage' },
+    { route: '/Chi siamo', label: 'Chi siamo' },
+    { route: '/Prodotti', label: 'Prodotti' }
+]
+const NavBar = () => {
+    return (
+        <>
+        <nav>
+            <ul>
+                {linkPage.map(link => (
+                    <li>
+                        <NavLink to={link.route}>{link.label}</NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+
+
+        </>
+    )
+}
+
+export default NavBar;
